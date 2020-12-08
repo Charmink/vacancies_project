f9f671c94363b6d07c400b374dc049c8233ad0b3
Charmink <mry2002@mail.ru>
2020-12-09 00:24:03 +0300

    deploy requirements fix

--- stepik_vacancies/stepik_vacancies/settings.py
+++ stepik_vacancies/stepik_vacancies/settings.py
@@ -19,10 +19,8 @@ DEBUG = True
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 STATIC_URL = "/static/"
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
