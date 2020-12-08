2de79d4b0a24bc33921abd058b0e41c4ec84a914
Charmink <mry2002@mail.ru>
2020-12-09 00:26:01 +0300

    root

--- stepik_vacancies/stepik_vacancies/settings.py
+++ stepik_vacancies/stepik_vacancies/settings.py
@@ -20,7 +20,7 @@ MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 STATIC_URL = "/static/"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
